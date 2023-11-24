5d0acc5ab26833d22e09e609a13baf817741efb7
rishissa <rishi.socialseller@gmail.com>
2023-11-24 10:44:21 +0000

    bugs fixed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,10 @@
 module.exports = {
-    apps: [
-      {
-        name: 'strapi',
-        cwd: '/root/reseller-strapi',
-        script: 'npm',
-        args: 'start',
-        env: {
-          NODE_ENV: 'development',
-          DATABASE_HOST: 'localhost', // database endpoint
-          DATABASE_PORT: '5432',
-          DATABASE_NAME: 'reseller_ssa', // DB name
-          DATABASE_USERNAME: 'resellerssa', // your username for psql
-          DATABASE_PASSWORD: 'resellerssa123', // your password for psql
-        },
-      },
-    ],
-  };
+  apps: [
+    {
+      name: "arham-backend",
+      script: "npm",
+      // args: "run develop",
+      args: "start",
+    },
+  ],
+};
